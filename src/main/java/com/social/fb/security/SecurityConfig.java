fe2b9886416bbaaea3440dee30309ec6736f2045
mourad404 <mourad.abdelah@gmail.com>
2018-08-25 21:34:54 +0200

    l'authentification par google+ est ajoutée

--- src/main/java/com/social/fb/security/SecurityConfig.java
+++ src/main/java/com/social/fb/security/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
 import org.springframework.social.connect.support.ConnectionFactoryRegistry;
 import org.springframework.social.connect.web.ProviderSignInController;
 import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.google.connect.GoogleConnectionFactory;
 
 @Configuration
 @EnableWebSecurity
@@ -41,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private UsersConnectionRepository usersConnectionRepository;
 
 	@Autowired
-	private FacebookConnectionSignup facebookConnectionSignup;
+	private SocialConnectionSignup socialConnectionSignup;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -61,6 +62,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public ConnectionFactoryLocator connectionFactoryLocator() {
 		ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+		registry.addConnectionFactory(new GoogleConnectionFactory(environment.getProperty("google.clientId"),
+				environment.getProperty("google.clientSecret")));
 		registry.addConnectionFactory(new FacebookConnectionFactory(environment.getProperty("facebook.clientId"),
 				environment.getProperty("facebook.clientSecret")));
 		return registry;
@@ -69,15 +72,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public UsersConnectionRepository usersConnectionRepository() {
 		InMemoryUsersConnectionRepository repository = new InMemoryUsersConnectionRepository(connectionFactoryLocator);
-		repository.setConnectionSignUp(facebookConnectionSignup);
+		repository.setConnectionSignUp(socialConnectionSignup);
 		return repository;
 	}
 
 	@Bean
 	public ProviderSignInController providerSignInController() {
-		((InMemoryUsersConnectionRepository) usersConnectionRepository).setConnectionSignUp(facebookConnectionSignup);
+		((InMemoryUsersConnectionRepository) usersConnectionRepository).setConnectionSignUp(socialConnectionSignup);
 		return new ProviderSignInController(connectionFactoryLocator, usersConnectionRepository,
-				new FacebookSignInAdapter());
+				new SocialSignInAdapter());
 	}
 
 }
